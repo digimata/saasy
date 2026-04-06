69c1865664ba9553995be414d33be8865c9d62b7
Andrew Jones <61884728+dremnik@users.noreply.github.com>
2026-04-06 12:59:13 -0700

    fix(auth): use controlled Select in invite member dialog
    Switch from defaultValue to value to prevent React warning about
    changing an uncontrolled component to controlled.

--- apps/web/components/auth/organization/invite-member-dialog.tsx
+++ apps/web/components/auth/organization/invite-member-dialog.tsx
@@ -192,7 +192,7 @@ export function InviteMemberDialog({
                   <FormItem>
                     <FormLabel className={classNames?.label}>{localization.ROLE}</FormLabel>
 
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <Select onValueChange={field.onChange} value={field.value}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue />
